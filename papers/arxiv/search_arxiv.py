81a04eafd10d9630f8818c3ef676cdede6e12e44
Ethan Kim <ethan_kim@college.harvard.edu>
2022-03-05 09:25:20 -0500

    update arxiv search script

--- papers/arxiv/search_arxiv.py
+++ papers/arxiv/search_arxiv.py
@@ -3,21 +3,24 @@ import argparse
 import json
 import urllib.request
 from arxiv import Search
-from datetime import datetime
+import datetime
 import logging
 
 
 def search_arxiv(query: str, category: str = "cs.CL", max_results: int = 200):
+    print(query)
     search = arxiv.Search(
         query=query,
         max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate,
     )
 
-    current_date = datetime.now().date()
+    current_date = datetime.datetime.now().date() - datetime.timedelta(days=1)
+    print(current_date)
     papers = []
     for result in search.results():
-        if category in result.categories and result.published == current_date:
+        print(result.published)
+        if category in result.categories and result.published.date() == current_date:
             papers.append(result)
     print(len(papers))
     return papers
@@ -26,7 +29,7 @@ def search_arxiv(query: str, category: str = "cs.CL", max_results: int = 200):
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", type=str)
-    parser.add_argument("--cateogory", type=str)
+    parser.add_argument("--category", type=str)
 
 
 if __name__ == "__main__":
