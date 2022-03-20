efa8ec9af873f0ce9cd2660e13559efd4606237a
Ethan Kim <ethan_kim@college.harvard.edu>
2022-03-20 15:39:46 -0400

    add logging

--- papers/arxiv/search_arxiv.py
+++ papers/arxiv/search_arxiv.py
@@ -8,21 +8,25 @@ import logging
 
 
 def search_arxiv(query: str, category: str = "cs.CL", max_results: int = 200):
-    print(query)
+    logging.info(
+        "Searching arxiv for papers with query {} in category {}".format(
+            query, category
+        )
+    )
     search = arxiv.Search(
         query=query,
         max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate,
     )
 
-    current_date = datetime.datetime.now().date() - datetime.timedelta(days=1)
-    print(current_date)
+    current_date = datetime.datetime.now().date() - datetime.timedelta(days=3)
+    logging.info("Searching for papers on ate: {}".format(current_date))
     papers = []
     for result in search.results():
         print(result.published)
         if category in result.categories and result.published.date() == current_date:
             papers.append(result)
-    print(len(papers))
+    logging.info("{} papers found".format(len(papers)))
     return papers
 
 
